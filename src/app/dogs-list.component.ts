540d13668b5b70b71c3dfe1d3d745352f494495c
kfedora <kfedora@protonmail.com>
2022-08-25 23:37:49 -0400

    update component html

--- src/app/dogs-list.component.ts
+++ src/app/dogs-list.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DogsService } from './dogs.service';
 
 @Component({
   selector: 'app-dogs-list',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      dogs-list works!
-    </p>
+    <section class="hero-section">
+      <h2 class="hero-text">
+        Discover pets to walk near you
+      </h2>
+    </section>
+    <article class="pet-list">
+      <ul>
+        <li *ngFor="let dog of dogsService.dogs">
+          {{dog.name}}
+          <p>
+            {{dog.description}}
+          </p>
+        </li>
+      </ul>
+    </article>
   `,
   styles: [
   ]
 })
 export class DogsListComponent implements OnInit {
 
-  constructor() { }
+  constructor(readonly dogsService: DogsService) { }
 
   ngOnInit(): void {
   }
