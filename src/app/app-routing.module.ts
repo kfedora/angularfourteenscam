c1390ec842398407dcec6b0ab062f492e95c8aa9
kfedora <kfedora@protonmail.com>
2022-08-25 23:25:12 -0400

    update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DogsListComponent } from './dogs-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'list' },
+  { path: 'list', component: DogsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
