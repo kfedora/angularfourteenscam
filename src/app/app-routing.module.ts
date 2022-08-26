7d7bcb4843206e014baf8eb025b53e2c246d7d5b
kfedora <kfedora@protonmail.com>
2022-08-26 01:08:30 -0400

    lazy load details view
    https://youtu.be/x5PZwb4XurU?t=522

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DogViewComponent } from './dog-view.component';
 import { DogsListComponent } from './dogs-list.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'list' },
   { path: 'list', component: DogsListComponent },
-  { path: 'details/:index', component: DogViewComponent }
+  { path: 'details/:index', loadComponent: () => import('./dog-view.component').then(m => m.DogViewComponent) }
 ];
 
 @NgModule({
