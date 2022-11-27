1b1f3f3b5ddf81b2bb51f90625ddcffcd177f99b
kfedora <kfedora@protonmail.com>
2022-11-26 21:35:04 -0500

    update update script to redirect stderr to stdout
    https://stackoverflow.com/a/876242/227646

--- update.sh
+++ update.sh
@@ -8,36 +8,36 @@ date > README.md;
 git add .;
 git commit -m "add timestamp";
 
-echo '' >> README.md;
-echo '# angular fourteen scam' >> README.md;
-echo '' >> README.md;
+echo '' >> README.md 2>&1;
+echo '# angular fourteen scam' >> README.md 2>&1;
+echo '' >> README.md 2>&1;
 
-echo '' >> README.md;
-echo 'This project is live at [https://angularfourteenscam.vercel.app/](https://angularfourteenscam.vercel.app/ "dog!") thanks to Github and Vercel.' >> README.md;
-echo '' >> README.md;
+echo '' >> README.md 2>&1;
+echo 'This project is live at [https://angularfourteenscam.vercel.app/](https://angularfourteenscam.vercel.app/ "dog!") thanks to Github and Vercel.' >> README.md 2>&1;
+echo '' >> README.md 2>&1;
 
-echo 'CI Status: ' >> README.md;
-echo '' >> README.md;
+echo 'CI Status: ' >> README.md 2>&1;
+echo '' >> README.md 2>&1;
 
-echo "\`\`\`bash" >> README.md;
+echo "\`\`\`bash" >> README.md 2>&1;
 date >> ping.txt;
 echo "" >> ping.txt;
 ping -A -D -c 20 -v 8.8.8.8 >> ping.txt;
 echo "" >> ping.txt;
 git add .;
 git commit -m "begin add system status";
-echo "System Memory" >> README.md;
-free -h >> README.md;
-echo "System Storage" >> README.md;
-du -sh .>> README.md;
+echo "System Memory" >> README.md 2>&1;
+free -h >> README.md 2>&1;
+echo "System Storage" >> README.md 2>&1;
+du -sh .>> README.md 2>&1;
 git add .;
 git commit -m "add system status";
-echo "\`\`\`" >> README.md;
+echo "\`\`\`" >> README.md 2>&1;
 git add .;
 git commit -m "end add system status";
 
 
-echo "\`\`\`bash" >> README.md;
+echo "\`\`\`bash" >> README.md 2>&1;
 date >> ping.txt;
 echo "" >> ping.txt;
 ping -A -D -c 20 -v 8.8.8.8 >> ping.txt;
@@ -45,9 +45,9 @@ echo "" >> ping.txt;
 git add .;
 git commit -m "begin update node";
 time npm install --global @angular/cli yarn;
-time yarn run ng --version >> README.md;
-time yarn >> README.md;
-echo "\`\`\`" >> README.md;
+time yarn run ng --version >> README.md 2>&1;
+time yarn >> README.md 2>&1;
+echo "\`\`\`" >> README.md 2>&1;
 date >> ping.txt;
 echo "" >> ping.txt;
 ping -A -D -c 20 -v 8.8.8.8 >> ping.txt;
@@ -60,14 +60,14 @@ git commit -m "begin prepare to update angular";time yarn run ng update @angular
 git add .;
 git commit -m "end prepare to update angular";
 
-echo "\`\`\`bash" >> README.md;
+echo "\`\`\`bash" >> README.md 2>&1;
 date >> ping.txt;
 echo "" >> ping.txt;
 ping -A -D -c 20 -v 8.8.8.8 >> ping.txt;
 echo "" >> ping.txt;
 git add .;
-git commit -m "begin prepare to build angular";time yarn run ng build --configuration production >> README.md;
-echo "\`\`\`" >> README.md;
+git commit -m "begin prepare to build angular";time yarn run ng build --configuration production >> README.md 2>&1;
+echo "\`\`\`" >> README.md 2>&1;
 git add .;
 git commit -m "end prepare to build angular";
 
@@ -83,12 +83,12 @@ ping -A -D -c 20 -v 8.8.8.8 >> ping.txt;
 git add .;
 git commit -m "end prepare to unit test angular";
 
-date >> README.md;
+date >> README.md 2>&1;
 date >> ping.txt;
 echo "" >> ping.txt;
 ping -D -c 20 -v 8.8.8.8 >> ping.txt;
 echo "" >> ping.txt;
-time yarn version --patch >> README.md;
+time yarn version --patch >> README.md 2>&1;
 git add .;
 git commit -m "add timestamp";
 git pull --rebase origin master --strategy-option=ours;
